bf4e51ac5e96b0379a309215e4dfb321a9ec0ddf
Documenter.jl <documenter@juliadocs.github.io>
2026-07-11 11:43:47 +0000

    build based on f7da75c

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"category":"section","location":"#SolverBenchmark","page":"Home","text":"Documentation for SolverBenchmark.\n\n","title":"SolverBenchmark"}]
+}
